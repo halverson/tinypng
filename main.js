b63f604c766e0cb3556c3939394e773db79059dd
Brent Nygaard <nygaardmeister@gmail.com>
2016-03-29 22:23:02 +0200

    Initialize git repo and add existing files.

---
+++ main.js
@@ -0,0 +1,18 @@
+//main.js
+
+var tinify =  require("tinify"),
+    fs = require("fs");
+
+tinify.key = "qW4HaKZhjU-8DgPLWXQW00IlKdVtJV1d";
+
+fs.readdir("src/", function (err, files) {
+    'use strict';
+    if (err) {
+        return console.error(err);
+    }
+    
+    files.forEach(function (file) {
+        var source = tinify.fromFile("src/" + file);
+        source.toFile("output/" + file);
+    });
+});

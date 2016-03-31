c7c9bf4f7a2d679ed8077123b2ebfd0b223c61c0
Brent Nygaard <nygaardmeister@gmail.com>
2016-03-31 14:39:36 +0200

    Fix formatting errors and change tinify key.

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-//main.js
+var tinify = require('tinify');
+var fs = require('fs');
 
-var tinify =  require("tinify"),
-    fs = require("fs");
+tinify.key = 'qW4HaKZhjU-8DgPLWXQW00IlKdVtJV1d';
 
-tinify.key = "qW4HaKZhjU-8DgPLWXQW00IlKdVtJV1d";
+fs.readdir('src/', function (err, files) {
+  'use strict';
+  if (err) {
+    return console.error(err);
+  }
 
-fs.readdir("src/", function (err, files) {
-    'use strict';
-    if (err) {
-        return console.error(err);
-    }
-    
-    files.forEach(function (file) {
-        var source = tinify.fromFile("src/" + file);
-        source.toFile("output/" + file);
-    });
+  files.forEach(function (file) {
+    var source = tinify.fromFile('src/' + file);
+    source.toFile('output/' + file);
+  });
 });
